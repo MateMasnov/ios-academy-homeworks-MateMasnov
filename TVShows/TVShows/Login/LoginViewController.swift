fefb0b690056892c172ccadd551c178259897092
MateMasnov <mate.masnov@fer.hr>
2018-07-17 19:43:47 +0200

    Refactored login screen for homework 3.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -2,65 +2,43 @@
 //  LoginViewController.swift
 //  TVShows
 //
-//  Created by Infinum Student Academy on 09/07/2018.
+//  Created by Infinum Student Academy on 17/07/2018.
 //  Copyright © 2018 Mate Masnov. All rights reserved.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
-    
-    var numberOfClicks: Int = 0;
-    let sleepTime: Int = 3;
-    
-    @IBOutlet weak var mySwitch: UISwitch!
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var incrementButton: UIButton!
-    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+
+    //MARK: - Outlets -
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkmarkButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        incrementButton.titleLabel?.adjustsFontSizeToFitWidth = true;
-        incrementButton.layer.cornerRadius = 20;
-        incrementButton.clipsToBounds = true;
-       
-        initializeAnimation(sleepTime: sleepTime);
+
         // Do any additional setup after loading the view.
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func touchUpInsideSwitchEvent(_ sender: UISwitch) {
-        switchControl();
-    }
-
-    func switchControl() -> Void {
-        if myActivityIndicator.isAnimating {
-            myActivityIndicator.stopAnimating()
-        } else {
-            myActivityIndicator.startAnimating();
-        }
+    //MARK: - IBActions -
+    @IBAction
+    func createAccountAction(_ sender: Any) {
+        
     }
     
-    @IBAction func TouchUpInsideButtonEvent(_ sender: Any) {
-        numberOfClicks += 1;
-        print("Value of the label is \(numberOfClicks)");
-        counterLabel.text = "Times clicked: \(numberOfClicks)";
+    @IBAction
+    func checkmarkAction(_ sender: Any) {
+        checkmarkButton.isSelected = !checkmarkButton.isSelected
     }
     
-    func initializeAnimation(sleepTime: Int) -> Void {
-        myActivityIndicator.startAnimating();
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(sleepTime), execute: {
-            if self.myActivityIndicator.isAnimating {
-                self.myActivityIndicator.stopAnimating();
-            }
-            self.mySwitch.setOn(false, animated: true);
-        })
-    }
     /*
     // MARK: - Navigation
 
@@ -70,4 +48,5 @@ class LoginViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
 }
